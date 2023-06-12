5c5b6c9b390afcbbb5d569dd205a15caf081f617
LAPTOP-DFIE4GI7\Flor Luna <Flor Luna>
2023-06-12 10:09:51 -0600

    Actualizar personas

--- src/main/java/mx/com/cuh/cuh/controller/RestController.java
+++ src/main/java/mx/com/cuh/cuh/controller/RestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -52,5 +53,10 @@ public class RestController {
 	public void insertarPersonas2() {
 		
 		System.out.println("eliminar");
+	}
+	
+	@PutMapping(value ="/insertarPersonas")
+	public void actualizarPersonas2() {
+		System.out.println("actualizar");
 	}
 }
