b891ab369c1825d5c08e83db620aeb95b49f935d
JaviHDZM <javierhdzm547@gmail.com>
2023-06-12 10:09:40 -0600

    :P

--- src/main/java/mx/com/cuh/cuh/controller/RestController.java
+++ src/main/java/mx/com/cuh/cuh/controller/RestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -48,9 +49,15 @@ public class RestController {
 		+ persona.getNombre());
 	}
 	
-	@DeleteMapping(value = "/insertarPersonas")
-	public void insertarPersonas2() {
+	@DeleteMapping(value = "/eliminarPersonas")
+	public void insertarPersonas() {
+	
 		
 		System.out.println("eliminar");
+	}
+	
+	@PutMapping(value = "/actualizarPersnas")
+	public void actualizarPersonas() {
+		System.out.println("Actualiza");
 	}
 }
