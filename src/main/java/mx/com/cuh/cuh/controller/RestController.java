2b910c54af068052e5b7243843b41071cff91e3c
JESUS AVILA <97324166+JesusAv01@users.noreply.github.com>
2023-06-13 10:41:49 -0600

    Se agrega logica para obtener personas como elimar

--- src/main/java/mx/com/cuh/cuh/controller/RestController.java
+++ src/main/java/mx/com/cuh/cuh/controller/RestController.java
@@ -3,35 +3,29 @@ package mx.com.cuh.cuh.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import mx.com.cuh.cuh.dto.PersonaDTO;
 import mx.com.cuh.cuh.dto.Respuesta;
+import mx.com.cuh.cuh.entity.TbPerson;
+import mx.com.cuh.cuh.service.Usuario;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 	
+	@Autowired
+	private Usuario usuario;
+	
 	@GetMapping(value = "/obtenerpersonas")
-	public Respuesta listaPersonas() {
+	public List<TbPerson> listaPersonas() {
 		
-		List<PersonaDTO> listasPersonas
-		= new ArrayList<PersonaDTO>();
-		
-		PersonaDTO persona1 = new PersonaDTO();
-		persona1.setNombre("juan");
-		persona1.setEdad(15);
-		
-		listasPersonas.add(persona1);
-		
-		Respuesta reponse = new Respuesta();
-		reponse.setListasPersona(listasPersonas);
-		reponse.setMensaje("Todo okas");
-		
-		return reponse; 
+		return usuario.obtenerPersonas(); 
 	}
 	
 	@GetMapping(value = "/obtenerinfo")
@@ -48,9 +42,14 @@ public class RestController {
 		+ persona.getNombre());
 	}
 	
-	@DeleteMapping(value = "/insertarPersonas")
-	public void insertarPersonas2() {
-		
-		System.out.println("eliminar");
+	@DeleteMapping(value = "/borrarPersona")
+	public void borrarPersonas() {
+		Long idPerson = 40269L;
+		usuario.borrarPersona(idPerson);
+	}
+	
+	@PutMapping(value = "/updateperson")
+	public void actualizarpersonas() {
+		System.out.println("ACTUALIZO");
 	}
 }
---
+++ src/main/java/mx/com/cuh/cuh/repository/TbPersonRepository.java
@@ -0,0 +1,16 @@
+package mx.com.cuh.cuh.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import mx.com.cuh.cuh.entity.TbPerson;
+
+@org.springframework.stereotype.Repository
+public interface TbPersonRepository
+extends CrudRepository<TbPerson, Long> {
+	//SELECT * FROM PERSON
+	List<TbPerson> findAll();
+	void deleteById(Long idPerson);
+}
