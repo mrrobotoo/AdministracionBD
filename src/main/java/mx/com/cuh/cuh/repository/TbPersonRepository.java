0f81032d25bedee7b68cc4dcf4516486511aaad2
Vane Guevara <cuh51208002@cuh.mx>
2023-06-14 09:23:32 -0600

    Cambios del 13 de Junio

---
+++ src/main/java/mx/com/cuh/cuh/repository/TbPersonRepository.java
@@ -0,0 +1,14 @@
+package mx.com.cuh.cuh.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mx.com.cuh.cuh.entity.TbPerson;
+
+@org.springframework.stereotype.Repository
+public interface TbPersonRepository 
+extends CrudRepository<TbPerson, Long> {
+	List<TbPerson> findAll();  //SELECT * FROM PERSON
+
+}
---
+++ src/main/java/mx/com/cuh/cuh/service/UsuarioImpl.java
@@ -0,0 +1,22 @@
+package mx.com.cuh.cuh.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mx.com.cuh.cuh.entity.TbPerson;
+import mx.com.cuh.cuh.repository.TbPersonRepository;
+
+@Service
+public class UsuarioImpl implements Usuario{
+	
+	@Autowired
+	private TbPersonRepository tbPersonRepository;
+
+	@Override
+	public List<TbPerson> obtenerPersonas() {
+		return tbPersonRepository.findAll();
+	}
+
+}
