68baad967f2f85e82a11c39170020f020970ba62
DESKTOP-Q5MIQCO\Adrian <adriannespi@gmail.com>
2023-06-14 09:23:13 -0600

    no message

---
+++ src/main/java/mx/com/cuh/cuh/controller/PersonaDTO.java
@@ -0,0 +1,19 @@
+package mx.com.cuh.cuh.controller;
+
+public class PersonaDTO {
+	private String nombre;
+	private Integer edad;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	
+}
---
+++ src/main/java/mx/com/cuh/cuh/service/UsuarioImpl.java
@@ -0,0 +1,6 @@
+package mx.com.cuh.cuh.service;
+
+public class UsuarioImpl implements usuario {
+ public void metodo() {
+}
+}
